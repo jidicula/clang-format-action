e7258cc7d8135436746a90e3f09f69cb33244628
jidicula <johanan.idicula@gmail.com>
2020-12-26 12:27:02 -0500

    [style][entrypoint.sh] Format with shfmt

--- entrypoint.sh
+++ entrypoint.sh
@@ -15,16 +15,16 @@
 # Accepts a filepath argument. The filepath passed to this function must point
 # to a .c or .h file. The file is formatted with clang-format and that output is
 # compared to the original file.
-format_diff(){
-    local filepath="$1"
-    local_format="$(/usr/bin/clang-format-10 -n --Werror --style=file --fallback-style=LLVM "${filepath}")"
-    local format_status="$?"
-    if [[ "${format_status}" -ne 0 ]]; then
-	echo "$local_format" >&2
-	exit_code=1
-	return "${format_status}"
-    fi
-    return 0
+format_diff() {
+	local filepath="$1"
+	local_format="$(/usr/bin/clang-format-10 -n --Werror --style=file --fallback-style=LLVM "${filepath}")"
+	local format_status="$?"
+	if [[ "${format_status}" -ne 0 ]]; then
+		echo "$local_format" >&2
+		exit_code=1
+		return "${format_status}"
+	fi
+	return 0
 }
 
 cd "$GITHUB_WORKSPACE" || exit 2
@@ -33,12 +33,12 @@ cd "$GITHUB_WORKSPACE" || exit 2
 exit_code=0
 
 # All files improperly formatted will be printed to the output.
-# find all C .c and .h files
+# find all C .c and .h files'
 c_files=$(find . -name "*.[hc]")
 
 # check formatting in each C file
 for file in $c_files; do
-    format_diff "${file}"
+	format_diff "${file}"
 done
 
 exit "$exit_code"
