5fd52f8619cbeb3e642fc34a03208fdee17cc7b6
MCWertGaming <dleven@protonmail.com>
2021-10-07 23:05:52 +0200

    fixed interactive mode still on

--- .ci/docker-run-test.sh
+++ .ci/docker-run-test.sh
@@ -8,14 +8,14 @@ if [ "$?" != "0" ]; then
 fi
 
 # should succeed
-docker run -e CLANG_FORMAT_VERSION=11 -e CHECK_PATH=known_pass -e EXCLUDE_REGEX=capi -e GITHUB_WORKSPACE=/test -it -v $(pwd)/test:/test --privileged clang-format-action-test
+docker run -e CLANG_FORMAT_VERSION=11 -e CHECK_PATH=known_pass -e EXCLUDE_REGEX=capi -e GITHUB_WORKSPACE=/test -v $(pwd)/test:/test --privileged clang-format-action-test
 if [ "$?" != "0" ]; then
     echo "files that should succeed have failed!"
     exit 1
 fi
 
 # should fail
-docker run -e CLANG_FORMAT_VERSION=11 -e CHECK_PATH=known_fail -e EXCLUDE_REGEX=capi -e GITHUB_WORKSPACE=/test -it -v $(pwd)/test:/test --privileged clang-format-action-test
+docker run -e CLANG_FORMAT_VERSION=11 -e CHECK_PATH=known_fail -e EXCLUDE_REGEX=capi -e GITHUB_WORKSPACE=/test -v $(pwd)/test:/test --privileged clang-format-action-test
 if [ "$?" == "0" ]; then
     echo "files that should fail have succeeded!"
     exit 2
