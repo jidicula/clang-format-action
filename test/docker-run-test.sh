051a3fdfa8bbea9f5938a162646f4fe4514a13a1
jidicula <johanan@forcepush.tech>
2021-11-06 20:19:06 -0400

    test: DRY run arguments

--- test/docker-run-test.sh
+++ test/docker-run-test.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env bash
 
+FALLBACK_STYLE="llvm"
+EXCLUDE_REGEX="capital"
+
 # build the docker container
 docker build . --file Dockerfile --tag clang-format-action-test --no-cache
 docker_status="$?"
@@ -9,7 +12,7 @@ if [[ "$docker_status" != "0" ]]; then
 fi
 
 # should succeed
-docker run -e CLANG_FORMAT_VERSION=13 -e GITHUB_WORKSPACE=/test -v "$(pwd)"/test:/test --privileged clang-format-action-test known_pass llvm capi
+docker run -e CLANG_FORMAT_VERSION=13 -e GITHUB_WORKSPACE=/test -v "$(pwd)"/test:/test --privileged clang-format-action-test known_pass $FALLBACK_STYLE $EXCLUDE_REGEX
 docker_status="$?"
 if [[ "$docker_status" != "0" ]]; then
 	echo "files that should succeed have failed!"
@@ -17,7 +20,7 @@ if [[ "$docker_status" != "0" ]]; then
 fi
 
 # should fail
-docker run -e CLANG_FORMAT_VERSION=13 -e GITHUB_WORKSPACE=/test -v "$(pwd)"/test:/test --privileged clang-format-action-test known_fail llvm capi
+docker run -e CLANG_FORMAT_VERSION=13 -e GITHUB_WORKSPACE=/test -v "$(pwd)"/test:/test --privileged clang-format-action-test known_fail $FALLBACK_STYLE $EXCLUDE_REGEX
 docker_status="$?"
 if [[ "$docker_status" == "0" ]]; then
 	echo "files that should fail have succeeded!"
