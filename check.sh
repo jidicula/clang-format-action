cc2e0ede48617474f4da55c502b6af11fe6c92b8
jidicula <git@johanan.dev>
2023-12-26 11:59:24 -0500

    perf(find): Use EXCLUDE_REGEX as find command 2nd regex arg

--- check.sh
+++ check.sh
@@ -101,14 +101,13 @@ src_files=$(find \
 	-o \
 	-regextype posix-egrep \
 	-regex "$INCLUDE_REGEX" \
+	-not -regex "$EXCLUDE_REGEX" \
 	-print)
 
 # check formatting in each source file
 IFS=$'\n' # Loop below should separate on new lines, not spaces.
 for file in $src_files; do
-	if ! [[ ${file} =~ $EXCLUDE_REGEX ]]; then
-		format_diff "${file}"
-	fi
+	format_diff "${file}"
 done
 
 # global exit code is flipped to nonzero if any invocation of `format_diff` has
